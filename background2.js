5456f736dc38c03c3c2d086728785e6bc341a7cc
danbi7184 <danbi7184@gmail.com>
2022-08-31 19:17:13 +0900

    img

--- background2.js
+++ background2.js
@@ -2,6 +2,10 @@ Module.register("background2", {
     defaults: {
     },
 
+    getStyles: function() {
+        return ["background2.css"]
+    },
+
     getDom: function() {
         var wrapper = document.createElement("div");
         var img = document.createElement("img");
