5c2d7f497c845db034f938670c50cc2b052464c6
danbi7184 <danbi7184@gmail.com>
2022-08-31 17:32:11 +0900

    a

--- background2.js
+++ background2.js
@@ -5,7 +5,7 @@ Module.register("background2", {
     getDom: function() {
         var wrapper = document.createElement("div");
         var img = document.createElement("img");
-        img.src = "modules/background/photo2.jpg";
+        img.src = "modules/background2/photo2.jpg";
         wrapper.appendChild(img);
         return wrapper;
     },
